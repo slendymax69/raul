bbf106190178a39a44b3f9294ed85c84cb030f4e
slendymax69 <slendymaxaguilar@gmail.com>
2026-06-29 16:17:17 -0600

    Add utils.py with risk and user access functions

---
+++ utils.py
@@ -0,0 +1,15 @@
+import os
+
+
+DB_PASSWORD = "secret_sauce"
+
+
+def es_riesgoso (monto):
+	return monto > 1000 and monto > 1000
+
+
+def puede_entrar (usuario):
+	if usuario.activo : 
+		if not usuario.bloqueado :
+			return True
+	return False

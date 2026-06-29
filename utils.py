b58b8e05fb88e53ee27170f4fad3ace311631aea
slendymax69 <slendymaxaguilar@gmail.com>
2026-06-29 16:41:00 -0600

    Refactor utils.py to use environment variable for DB_PASSWORD

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 import os
 
 
-DB_PASSWORD = "secret_sauce"
+DB_PASSWORD = os.environ["secret_sauce"]
 
 
 def es_riesgoso (monto):
-	return monto > 1000 and monto > 1000
+	return monto > 1000
 
 
 def puede_entrar (usuario):
-	if usuario.activo : 
-		if not usuario.bloqueado :
+	if usuario.activo and not usuario.bloqueado : 
 			return True
 	return False
